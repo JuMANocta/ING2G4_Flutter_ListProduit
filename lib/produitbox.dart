43ebbd2b371f58aba68613e3c8de5178c5dfda62
JuMANocta <julien.mangeat@reseau-cd.net>
2022-03-17 17:20:56 +0100

    fix bug (required)

--- lib/produitbox.dart
+++ lib/produitbox.dart
@@ -4,35 +4,33 @@ import 'ratingbox.dart';
 
 class ProduitBox extends StatelessWidget {
   final Produit item;
-  ProduitBox({Key? key, this.item}) : super(key: key);
-  
+  ProduitBox({Key? key, required this.item}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(2),
-      height: 140,
-      child: Card(
-        child: Row(
+        padding: const EdgeInsets.all(2),
+        height: 140,
+        child: Card(
+            child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Image.asset(item.image),
             Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(5),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(item.name, style: const TextStyle(fontWeight: FontWeight.bold)),
-                    Text(item.description),
-                    Text("Prix : " + item.prix.toString()),
-                    RatingBox(),
-                  ],
-                )
-              )
-            ),
+                child: Container(
+                    padding: const EdgeInsets.all(5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(item.name,
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                        Text(item.description),
+                        Text("Prix : " + item.prix.toString()),
+                        RatingBox(),
+                      ],
+                    ))),
           ],
-        )
-      )
-    )
+        )));
   }
 }
